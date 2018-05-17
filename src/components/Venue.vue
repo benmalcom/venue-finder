d3b3da47d8c5bba3055edcee012dd42c13cc96d8
Benjamin Ikeji <benjaminikeji@gmail.com>
2018-05-17 21:23:05 +0100

    Created Venue  component

---
+++ src/components/Venue.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-md-4 col-sm-6">
+        <div class="row venue-item col-sm-12 col-12 text-right shadow">
+            <div class="col-md-3 col-3 center-content">
+                <div class="ml-auto circle circle-md center-content business-image-placeholder">
+                    <img src="images/venue-marker.png" class="full-height full-width">
+                </div>
+            </div>
+            <div class="col-md-9 col-9 center-content pl-0">
+                <div class="mr-auto text-left">
+                    <h6 class="mb-1 mt-1 text-md">
+                        <strong>
+                            Victoria's Secret
+                        </strong>
+                    </h6>
+                    <h6 class="text-sm text-muted">
+                        174/194 Obafemi Awolowo way,
+                        Alausa, Ikeja, Lagos state.
+                    </h6>
+                    <span class="badge badge-warning text-white">SALON</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: 'Venue'
+	}
+</script>
+
+<style scoped>
+
+</style>
