d3ec50ce8d36dfdfa131107eb3e025d440e703c4
Benjamin Ikeji <benjaminikeji@gmail.com>
2018-05-19 17:20:18 +0100

    fixed redirect loop on server

--- server.js
+++ server.js
@@ -3,18 +3,7 @@ const express = require('express');
 const http = require('http');
 const serveStatic = require('serve-static');
 app = express();
-app.use(requireHTTPS);
 app.use(serveStatic(__dirname + '/dist'));
-
-
 const server = http.createServer(app);
 const port = process.env.PORT || 8000;
 server.listen(port);
-
-function requireHTTPS(req, res, next) {
-	if (!req.secure) {
-		return res.redirect('https://' + req.get('host') + req.url);
-		//FYI this should work for local development as well
-	}
-	return next();
-}
